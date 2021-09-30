03b486aa07b4e41568c5f215f594f473655e47ea
thebilderberg <mygooglegirls@yandex.ru>
2021-09-30 14:24:43 +0300

    vol.2

--- evgenStyleScript.js
+++ evgenStyleScript.js
@@ -1,23 +1,17 @@
 
-
-function incMenu(inc_m) {
-    let window = document.querySelector(inc_m);
-    window.hidden = true;
-}
-
 function incButton(inc_b, inc_m) {
     let button_inc = document.querySelector(inc_b);
     button_inc.addEventListener('click', () => {
         let window_inc = document.querySelector(inc_m);
-        if (window_inc.hidden === true) {
-            window_inc.hidden = false;
+        if (window_inc.classList.contains("hide")) {
+            window_inc.classList.remove("hide");
         } else {
-            window_inc.hidden = true;
+            window_inc.classList.add("hide");
         }
     });
 }
 
-incMenu('.incognitoMenu');
 incButton('.burgerButton', '.incognitoMenu');
-incMenu('.cheezBurgerInfo');
 incButton('.cheezBurgerButton', '.cheezBurgerInfo');
+
+
